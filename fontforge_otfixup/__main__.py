6742eae662b722ecb9fe7dae980f40b52125b729
MihailJP <mihailjp@gmail.com>
2026-03-08 01:21:39 +0900

    Add post.isFixedPitch correction hook
    Configuration feature is to do

--- fontforge_otfixup/__main__.py
+++ fontforge_otfixup/__main__.py
@@ -1,5 +1,42 @@
 import fontforge
-from . import smartDropout
+from . import postIsFixedPitchHook, smartDropout
+from typing import Literal, Callable
+
+
+def _addGlobalHook(
+    name: Literal['newFontHook', 'loadFontHook'],
+    hook: Callable[[fontforge.font], None]
+):
+    assert isinstance(fontforge.hooks, dict)
+    if name in fontforge.hooks:
+        currentHook = fontforge.hooks[name]
+
+        def chainHook(font: fontforge.font):
+            currentHook(font)
+            hook(font)
+
+        fontforge.hooks[name] = chainHook
+    else:
+        fontforge.hooks[name] = hook
+
+
+def _addFontHook(
+    font: fontforge.font,
+    name: Literal['generateFontPreHook', 'generateFontPostHook'],
+    hook: Callable[[fontforge.font, str], None]
+):
+    if not isinstance(font.temporary, dict):
+        font.temporary = {}
+    if name in font.temporary:
+        currentHook = font.temporary[name]
+
+        def chainHook(font: fontforge.font, target: str):
+            currentHook(font, target)
+            hook(font, target)
+
+        font.temporary[name] = chainHook
+    else:
+        font.temporary[name] = hook
 
 
 def fontforge_plugin_init(**kw):
@@ -9,3 +46,12 @@ def fontforge_plugin_init(**kw):
         context="Font",
         name="Activate smart dropout"
     )
+
+    def generateHook(font: fontforge.font, target: str):
+        postIsFixedPitchHook.fixPostIsFixedPitch(font, target)
+
+    def openHook(font: fontforge.font):
+        _addFontHook(font, 'generateFontPostHook', generateHook)
+
+    _addGlobalHook('newFontHook', openHook)
+    _addGlobalHook('loadFontHook', openHook)
---
+++ fontforge_otfixup/postIsFixedPitchHook.py
@@ -0,0 +1,63 @@
+import fontforge
+from fontTools import ttLib, ufoLib
+from typing import Iterable
+
+
+def _isFixedPitch(font: fontforge.font) -> bool:
+    return len(
+        set(
+            (glyph.width for glyph in font.glyphs() if 0x20 <= glyph.unicode <= 0x7e)
+        )
+    ) == 1
+
+
+class _ufoInfo2:
+    from fontTools.ufoLib import fontInfoAttributesVersion2
+    from itertools import repeat
+    _data = dict(zip(list(fontInfoAttributesVersion2), repeat(None)))
+
+    def __getattr__(self, name):
+        if name in self._data:
+            return self._data[name]
+        else:
+            raise AttributeError("attribute '{0}' not found".format(name))
+
+    def __setattr__(self, name, value):
+        if name in self._data:
+            self._data[name] = value
+        else:
+            raise AttributeError("attribute '{0}' not found".format(name))
+
+
+class _ufoInfo3(_ufoInfo2):
+    from fontTools.ufoLib import fontInfoAttributesVersion3
+    from itertools import repeat
+    _data = dict(zip(list(fontInfoAttributesVersion3), repeat(None)))
+
+
+def _fixPostIsFixedPitch_ttf(font: fontforge.font, target: str):
+    # TODO: configuration to enable or disable
+    with ttLib.TTFont(target) as ttf:
+        ttf['post'].isFixedPitch = 1
+        ttf.save(target)
+
+
+def _fixPostIsFixedPitch_ufo(font: fontforge.font, target: str):
+    # TODO: configuration to enable or disable
+    with ufoLib.UFOReaderWriter(target) as ufo:
+        info = _ufoInfo3() if ufo.formatVersionTuple[0] >= 3 else _ufoInfo2()
+        ufo.readInfo(info)
+        ufo.postscriptIsFixedPitch = True
+        ufo.writeInfo(info)
+
+
+def _checkExtension(filename: str, extensions: Iterable[str]) -> bool:
+    return any((filename.endswith(ex) for ex in extensions))
+
+
+def fixPostIsFixedPitch(font: fontforge.font, target: str):
+    if _isFixedPitch(font):
+        if _checkExtension(target, ['.ttf', '.otf']):
+            _fixPostIsFixedPitch_ttf(font, target)
+        elif _checkExtension(target, ['.ufo', '.ufo2', '.ufo3']):
+            _fixPostIsFixedPitch_ufo(font, target)
